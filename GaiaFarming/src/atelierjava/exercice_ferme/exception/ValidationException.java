472f1f566e0d82c5b776ef501ffbe85db53cfa33
Renaud BERNARD <renaud.bernard@p59production.com>
2018-02-28 10:23:19 +0100

    Create 2 class
    Create 2 class for Validation Exception & Pseudo Exist Exception.

--- GaiaFarming/src/atelierjava/exercice_ferme/exception/ValidationException.java
+++ GaiaFarming/src/atelierjava/exercice_ferme/exception/ValidationException.java
@@ -9,13 +9,6 @@ package atelierjava.exercice_ferme.exception;
  *
  * @author renob
  */
-public class AtelierjavaExercice_fermeException {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class ValidationException extends Exception{
     
 }
