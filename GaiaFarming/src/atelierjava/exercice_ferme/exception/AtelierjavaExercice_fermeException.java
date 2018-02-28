bb1251830f78d0f814afee049d81032c3d698ab8
Renaud BERNARD <renaud.bernard@p59production.com>
2018-02-28 10:10:59 +0100

    Creat Exception class
    Create cpackage for exceptions.

---
+++ GaiaFarming/src/atelierjava/exercice_ferme/exception/AtelierjavaExercice_fermeException.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atelierjava.exercice_ferme.exception;
+
+/**
+ *
+ * @author renob
+ */
+public class AtelierjavaExercice_fermeException {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
